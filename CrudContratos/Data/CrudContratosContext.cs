0af6bc5daa11e5480c64729ed74dc863c5024157
Laura Porras <laura.porras@crowe.com.co>
2025-08-04 10:44:52 -0500

    primer commit

---
+++ CrudContratos/Data/CrudContratosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CrudContratos.Models;
+
+namespace CrudContratos.Data
+{
+    public class CrudContratosContext : DbContext
+    {
+        public CrudContratosContext (DbContextOptions<CrudContratosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudContratos.Models.Contratos> Contratos { get; set; } = default!;
+    }
+}
---
+++ CrudContratos/Models/Contratos.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CrudContratos.Models
+{
+    public class Contratos
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del Contrato")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Fecha de Inicio")]
+        [DataType(DataType.Date)]
+        public DateTime FechaInicio { get; set; }
+
+        [Display(Name = "Fecha de Fin")]
+        [DataType(DataType.Date)]
+        public DateTime FechaFin { get; set; }
+
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+    }
+}
